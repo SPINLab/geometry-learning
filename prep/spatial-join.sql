54d55c125ee2400c3a8c1230cfaaed7ad220a8b3
reinvantveer <rein.van.t.veer@geodan.nl>
2017-07-27 16:18:17 +0200

    Refactor

--- prep/spatial-join.sql
+++ prep/spatial-join.sql
@@ -1,3 +1,9 @@
-SELECT st_astext(gebouw.wkb_geometry) AS source_wkt, st_astext(osm_buildings.wkb_geometry) AS target_wkt, st_astext(st_intersection(gebouw.wkb_geometry, osm_buildings.wkb_geometry)) AS intersection_wkt
+SELECT st_astext(st_snaptogrid(gebouw.wkb_geometry, 0.000001)) AS brt_wkt,
+  st_astext(st_snaptogrid(osm_buildings.wkb_geometry, 0.000001)) AS osm_wkt,
+  st_astext(st_snaptogrid(st_intersection(gebouw.wkb_geometry, osm_buildings.wkb_geometry), 0.000001)) AS intersection_wkt
 FROM gebouw, osm_buildings
-WHERE st_intersects(st_buffer(gebouw.wkb_geometry, 0.00005), osm_buildings.wkb_geometry);
+WHERE st_intersects(st_buffer(gebouw.wkb_geometry, 0.00005), osm_buildings.wkb_geometry) AND
+	st_issimple(st_snaptogrid(gebouw.wkb_geometry, 0.000001)) AND
+	st_issimple(st_snaptogrid(osm_buildings.wkb_geometry, 0.000001)) AND
+	st_issimple(st_snaptogrid(st_intersection(gebouw.wkb_geometry, osm_buildings.wkb_geometry), 0.000001))
+LIMIT 100000;
