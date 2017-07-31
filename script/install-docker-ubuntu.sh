9a44cc988503c3d22c872270c32e121711b87cd6
reinvantveer <rein.van.t.veer@geodan.nl>
2017-07-31 16:25:12 +0200

    Docker setup script

---
+++ script/install-docker-ubuntu.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+sudo apt-get update
+
+# For Ubuntu version 14.04 (Trusty)
+eval $(cat /etc/*release | grep VERSION_ID)
+
+if [ VERSION_ID = "14.04" ]; then
+  sudo apt-get -y install linux-image-extra-$(uname -r) linux-image-extra-virtual
+fi
+
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get -y install docker-ce
+sudo pip install docker-compose
