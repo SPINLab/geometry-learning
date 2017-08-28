2aebdfc188d3494dc07c15ee4f3c98d35214e08f
reinvantveer <rein.van.t.veer@geodan.nl>
2017-08-28 16:25:39 +0200

    separate extract script, extract centroids

--- prep/spatial-join.sql
+++ prep/spatial-join.sql
@@ -2,7 +2,11 @@ SELECT st_astext(st_snaptogrid(gebouw.wkb_geometry, 0.000001)) AS brt_wkt,
   st_astext(st_snaptogrid(ST_GeometryN(osm_buildings.wkb_geometry, 1), 0.000001)) AS osm_wkt,
   st_astext(st_snaptogrid(st_intersection(gebouw.wkb_geometry, osm_buildings.wkb_geometry), 0.000001)) AS intersection_wkt,
   st_distance(st_transform(st_centroid(gebouw.wkb_geometry), 28992), st_transform(st_centroid(osm_buildings.wkb_geometry), 28992)) AS centroid_distance,
-  st_distance(st_transform(gebouw.wkb_geometry, 28992), st_transform(osm_buildings.wkb_geometry, 28992)) AS geom_distance
+  st_distance(st_transform(gebouw.wkb_geometry, 28992), st_transform(osm_buildings.wkb_geometry, 28992)) AS geom_distance,
+  st_astext(st_snaptogrid(st_centroid(gebouw.wkb_geometry), 0.000001)) AS brt_centroid_wkt,
+  st_astext(st_snaptogrid(st_centroid(osm_buildings.wkb_geometry), 0.000001)) AS osm_centroid_wkt,
+  st_astext(st_snaptogrid(st_transform(st_centroid(gebouw.wkb_geometry), 28992), 0.000001)) AS brt_centroid_wkt_rd,
+  st_astext(st_snaptogrid(st_transform(st_centroid(osm_buildings.wkb_geometry), 28992), 0.000001)) AS osm_centroid_wkt_rd
 FROM gebouw, osm_buildings
 WHERE
   -- Allow only polygons (there are a few point buildings in there, don't ask me why)
@@ -16,4 +20,4 @@ WHERE
 	-- Restrict to ringless polygons
 	ST_NumInteriorRings(gebouw.wkb_geometry) = 0 AND
 	ST_NumInteriorRings(ST_GeometryN(osm_buildings.wkb_geometry, 1)) = 0
-LIMIT 100000;
+LIMIT 500000
