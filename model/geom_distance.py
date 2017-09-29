294d87d8347a3c12161fae6196fb22f10dd63b9c
reinvantveer <rein.van.t.veer@geodan.nl>
2017-09-29 14:16:21 +0200

    Refactor

--- model/geom_distance.py
+++ model/geom_distance.py
@@ -7,9 +7,9 @@ from keras.engine import Model
 from keras.layers import LSTM, Dense
 from keras.optimizers import Adam
 
-from topoml_util.CustomCallback import CustomCallback
+from topoml_util.ConsoleLogger import DecypherAll
 from topoml_util.geom_loss import gaussian_1d_loss
-from topoml_util.geom_scaler import localized_normal
+from topoml_util.geom_scaler import localized_normal, localized_mean
 
 # To suppress tensorflow info level messages:
 # export TF_CPP_MIN_LOG_LEVEL=2
@@ -24,8 +24,10 @@ OPTIMIZER = 'adam'
 
 loaded = np.load(DATA_FILE)
 training_vectors = loaded['input_geoms']
-# Bring coordinates and distance in roughly the same scale
-training_vectors = localized_normal(training_vectors, 1e4)
+
+# Bring coordinates and distance in the same scale
+means = localized_mean(training_vectors)
+training_vectors = localized_normal(training_vectors, means)
 
 (data_points, max_points, GEO_VECTOR_LEN) = training_vectors.shape
 target_vectors = loaded['geom_distance'][:, 0, :]
@@ -38,7 +40,7 @@ model.compile(loss=gaussian_1d_loss, optimizer=Adam(lr=0.005))
 model.summary()
 
 tb_callback = TensorBoard(log_dir='./tensorboard_log/' + TIMESTAMP, histogram_freq=1, write_graph=True)
-my_callback = CustomCallback(lambda x: str(x))
+my_callback = DecypherAll(lambda x: str(x))
 
 history = model.fit(x=training_vectors,
                     y=target_vectors,
@@ -47,4 +49,22 @@ history = model.fit(x=training_vectors,
                     validation_split=TRAIN_VALIDATE_SPLIT,
                     callbacks=[my_callback, tb_callback]).history
 
-print(history)
+prediction = model.predict(training_vectors[0:1000])
+
+intersecting_target = []
+intersecting_prediction = []
+non_intersecting_target = []
+non_intersecting_prediction = []
+
+for index, _ in enumerate(target_vectors[0:1000]):
+    if target_vectors[index, 0] == 0.:
+        intersecting_target.append(target_vectors[index])
+        intersecting_prediction.append(prediction[index])
+    else:
+        non_intersecting_target.append(target_vectors[index])
+        non_intersecting_prediction.append(prediction[index])
+
+intersecting_error = np.abs(np.array(intersecting_prediction)[:, 0] - np.array(intersecting_target)[:, 0])
+non_intersecting_error = np.abs(np.array(non_intersecting_prediction)[:, 0] - np.array(non_intersecting_target)[:, 0])
+print('Intersecting error factor:', np.mean(intersecting_error))
+print('Non-intersecting error factor:', np.mean(non_intersecting_error))
