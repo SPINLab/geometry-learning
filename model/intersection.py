f81a69af6ef7a170245b84d2e47e18bc667755e1
reinvantveer <rein.van.t.veer@geodan.nl>
2017-10-04 13:01:32 +0200

    Refactor

--- model/intersection.py
+++ model/intersection.py
@@ -5,11 +5,9 @@ from keras import Input
 from keras.callbacks import TensorBoard
 from keras.engine import Model
 from keras.layers import LSTM, TimeDistributed, Dense
-from keras.optimizers import Adam, sgd
-
-from topoml_util.ConsoleLogger import DecypherAll
-from topoml_util.GeoVectorizer import GeoVectorizer, FULL_STOP_INDEX
+from keras.optimizers import Adam
 from topoml_util.GaussianMixtureLoss import GaussianMixtureLoss
+from topoml_util.PyplotLogger import DecypherAll
 from topoml_util.geom_scaler import localized_normal, localized_mean
 
 # To suppress tensorflow info level messages:
@@ -17,14 +15,12 @@ from topoml_util.geom_scaler import localized_normal, localized_mean
 
 TIMESTAMP = str(datetime.now()).replace(':', '.')
 DATA_FILE = '../files/geodata_vectorized.npz'
-BATCH_SIZE = 512
-GAUSSIAN_MIXTURE_COMPONENTS = 5
+BATCH_SIZE = 1024
+GAUSSIAN_MIXTURE_COMPONENTS = 1
 TRAIN_VALIDATE_SPLIT = 0.1
 LATENT_SIZE = 128
 EPOCHS = 50
 OPTIMIZER = Adam(lr=1e-4)
-
-lossfunc = GaussianMixtureLoss(GAUSSIAN_MIXTURE_COMPONENTS)
 
 loaded = np.load(DATA_FILE)
 raw_training_vectors = loaded['input_geoms']
@@ -42,8 +38,11 @@ for train, target in zip(raw_training_vectors, raw_target_vectors):
 means = localized_mean(training_vectors)
 training_vectors = localized_normal(training_vectors, means, 1e4)
 target_vectors = localized_normal(target_vectors, means, 1e4)
-(data_points, max_points, INPUT_VECTOR_LEN) = training_vectors.shape
 
+(data_points, _, INPUT_VECTOR_LEN) = training_vectors.shape
+(_, max_points, OUTPUT_VECTOR_LEN) = target_vectors.shape
+
+# Expand the target vectors to gaussian mixture model size compatible with the prediction format
 component_1 = target_vectors[:, :, 0:5]  # The first gaussian mixture model component for each point
 component_1 = np.append(component_1, np.zeros((data_points, max_points, 1)), axis=2)  # Add pi feature
 target_vectors = np.append(
@@ -51,18 +50,20 @@ target_vectors = np.append(
         np.repeat(component_1, GAUSSIAN_MIXTURE_COMPONENTS, axis=1),
         (data_points, max_points, 6 * GAUSSIAN_MIXTURE_COMPONENTS)),
     target_vectors[:, :, 5:], axis=2)
-(_, _, OUTPUT_VECTOR_LEN) = target_vectors.shape
+
 
 inputs = Input(shape=(max_points, INPUT_VECTOR_LEN))
 model = LSTM(LATENT_SIZE, activation='relu', return_sequences=True)(inputs)
 model = TimeDistributed(Dense(64))(model)
 model = Dense(OUTPUT_VECTOR_LEN)(model)
 model = Model(inputs, model)
-model.compile(loss=lossfunc.geom_gaussian_mixture_loss, optimizer=OPTIMIZER)
+model.compile(
+    loss=GaussianMixtureLoss(GAUSSIAN_MIXTURE_COMPONENTS, max_points).geom_gaussian_mixture_loss,
+    optimizer=OPTIMIZER)
 model.summary()
 
 tb_callback = TensorBoard(log_dir='./tensorboard_log/' + TIMESTAMP, histogram_freq=1, write_graph=True)
-my_callback = DecypherAll(lambda x: str(x))
+decypher = DecypherAll(gmm_size=GAUSSIAN_MIXTURE_COMPONENTS, )
 
 history = model.fit(
     x=training_vectors,
@@ -70,7 +71,7 @@ history = model.fit(
     epochs=EPOCHS,
     batch_size=BATCH_SIZE,
     validation_split=TRAIN_VALIDATE_SPLIT,
-    callbacks=[my_callback, tb_callback]
+    callbacks=[decypher, tb_callback]
 ).history
 
 print(history)
