76af870ad75648155f2820bef0f52bc47fac8b66
reinvantveer <rein.van.t.veer@geodan.nl>
2017-10-18 08:48:06 +0000

    test gaussian sample plotting

--- model/topoml_util/test_wkt2pyplot.py
+++ model/topoml_util/test_wkt2pyplot.py
@@ -1,6 +1,11 @@
 import unittest
+import numpy as np
 from datetime import datetime
-from wkt2pyplot import wkt2pyplot
+
+from shapely.geometry import Point
+from topoml_util.wkt2pyplot import wkt2pyplot
+
+from topoml_util.GeoVectorizer import GeoVectorizer
 
 
 class TestWktToPyplotPoly(unittest.TestCase):
@@ -35,4 +40,30 @@ class TestWktToPyplotPoly(unittest.TestCase):
         plt.text(0.01, 0.06, 'prediction: some more text', transform=ax.transAxes)
         plt.text(0.01, 0.01, 'target: some text', transform=ax.transAxes)
 
+        plt.show()
+
+    def test_gaussian_sample_plot(self):
+
+        inputs = 'POLYGON((1.09872727273 -0.289454545452,-0.241272727273 0.682545454538,-0.992272727274 ' \
+                 '0.292545454528,0.347727272727 -0.680454545474,1.09872727273 -0.289454545452))\nPOLYGON((' \
+                 '-0.976272727273 0.302545454574,-0.25627272727 0.676545454539,1.05372727273 -0.276454545443,' \
+                 '0.320727272731 -0.654454545455,-0.477272727268 -0.0664545454754,-0.976272727273 ' \
+                 '0.302545454574))'
+        inputs = inputs.split('\n')
+
+        target = np.array([
+            # mu1 mu2  s1  s2  rho pi  [geo type one-hot            ]  [render 1hot]
+            [0.1,   0.1, 0.1, 0.1, 0., 0., 0., 0., 0., 1., 0., 0., 0., 0., 1., 0., 0., ],
+            [0.1,  -0.1, 0.1, 0.1, 0., 0., 0., 0., 0., 1., 0., 0., 0., 0., 1., 0., 0., ],
+            [-0.1, -0.1, 0.1, 0.1, 0., 0., 0., 0., 0., 1., 0., 0., 0., 0., 1., 0., 0., ],
+            [-0.1,  0.1, 0.1, 0.1, 0., 0., 0., 0., 0., 1., 0., 0., 0., 0., 1., 0., 0., ],
+            [0.1,   0.1, 0.1, 0.1, 0., 0., 0., 0., 0., 1., 0., 0., 0., 0., 0., 0., 1., ],
+            [0.,     0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 1., ],
+            [0.,     0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 1., ]
+        ])
+
+        target = [Point(point).wkt for point in
+                  GeoVectorizer(gmm_size=1).decypher_gmm_geom(target, 1000)]
+
+        plt, fig, ax = wkt2pyplot(inputs, target, None)
         plt.show()
