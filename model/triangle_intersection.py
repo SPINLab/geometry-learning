01dea2cd1a3df29b8a3d8bfb719dcf825fb78d92
reinvantveer <rein.van.t.veer@geodan.nl>
2017-10-18 09:06:37 +0000

    archive config, plot end results

--- model/triangle_intersection.py
+++ model/triangle_intersection.py
@@ -1,50 +1,45 @@
+import os
 from datetime import datetime
-
 from shapely.wkt import loads
 from shapely.geometry import Polygon
 import numpy as np
 from keras import Input
 from keras.callbacks import TensorBoard
 from keras.engine import Model
-from keras.layers import LSTM, TimeDistributed, Dense, Flatten, LeakyReLU
-from keras.optimizers import Adam, sgd
+from keras.layers import LSTM, Dense
+from keras.optimizers import Adam
+from shutil import copyfile
 
 from topoml_util.LoggerCallback import EpochLogger
 from topoml_util.GeoVectorizer import GeoVectorizer
 from topoml_util.geom_loss import geom_gaussian_loss
 from topoml_util.wkt2pyplot import wkt2pyplot
 
+SCRIPT_NAME = os.path.basename(__file__)
 TIMESTAMP = str(datetime.now()).replace(':', '.')
+DATA_FILE = '../files/triangles.npz'
 BATCH_SIZE = 2048
 TRAIN_VALIDATE_SPLIT = 0.1
 LATENT_SIZE = 64
 EPOCHS = 400
-OPTIMIZER = Adam(lr=1e-3, decay=1e-4)
+OPTIMIZER = Adam(lr=1e-3, decay=2e-5)
 
-print('Creating triangles')
-raw_training_vectors = np.random.normal(size=(100000, 6, 2))
-triangle_sets = np.array([[Polygon(point_set[0:3]).wkt, Polygon(point_set[3:]).wkt]
-                          for point_set in raw_training_vectors])
-max_points = GeoVectorizer.max_points(triangle_sets[:, 0], triangle_sets[:, 1])
-raw_training_vectors = [GeoVectorizer.vectorize_two_wkts(*triangle_set, max_points)
-                        for triangle_set in triangle_sets]
+# Archive the configuration
+copyfile(__file__, 'configs/' + TIMESTAMP + ' ' + SCRIPT_NAME)
+
+
+loaded = np.load(DATA_FILE)
+training_vectors = loaded['point_sequence']
+target_vectors = loaded['intersection_geoms']
+(set_size, _) = training_vectors.shape
+training_vectors = np.reshape(training_vectors, (set_size, 1, 12))
+target_triangles = [GeoVectorizer.vectorize_wkt(triangle, 6)
+                    for triangle in training_vectors]
 # triangles = [GeoVectorizer.interpolate(point_sequence, len(point_sequence) * 20)
 #                         for point_sequence in triangles]
-(_, max_points, GEO_VECTOR_LEN) = np.array(raw_training_vectors).shape
+(_, max_points, GEO_VECTOR_LEN) = np.array(target_vectors).shape
 
-print('Intersecting triangles and pruning')
-training_vectors = []
-target_vectors = []
-for index, (a, b) in enumerate(triangle_sets):
-    if loads(a).intersection(loads(b)).type == 'Polygon':
-        intersection = loads(a).intersection(loads(b)).wkt
-        target_vectors.append(GeoVectorizer.vectorize_wkt(intersection, max_points))
-        training_vectors.append(raw_training_vectors[index])
-
-training_vectors = np.array(training_vectors)
-target_vectors = np.array(target_vectors)
-
-inputs = Input(shape=(max_points, GEO_VECTOR_LEN))
+inputs = Input(shape=training_vectors.shape[1:])
 model = LSTM(LATENT_SIZE, activation='relu', return_sequences=True)(inputs)
 model = Dense(32, activation='relu')(model)
 model = LSTM(LATENT_SIZE, activation='relu', return_sequences=True)(model)
@@ -71,3 +66,13 @@ model.fit(
     validation_split=TRAIN_VALIDATE_SPLIT,
     callbacks=[epoch_callback, tb_callback]
 )
+
+plot_sample = training_vectors[-10000:]
+prediction = model.predict(plot_sample)
+intersecting_error = np.abs(prediction[:, 0] - target_vectors[-10000:])
+
+triangle_vectors = plot_sample.reshape(10000, 6, 2)
+training_triangles = np.array([[Polygon(point_set[0:3]).wkt, Polygon(point_set[3:]).wkt]
+                               for point_set in triangle_vectors])
+
+print('Done!')
