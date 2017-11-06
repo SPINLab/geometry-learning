465746dbbca0ed62889f115186bbf39702037210
reinvantveer <rein.van.t.veer@geodan.nl>
2017-11-06 08:24:26 +0100

    plot target distribution, switch leaky relu for relu

--- model/geom_distance.py
+++ model/geom_distance.py
@@ -5,7 +5,7 @@ import numpy as np
 from keras import Input
 from keras.callbacks import TensorBoard, EarlyStopping
 from keras.engine import Model
-from keras.layers import LSTM, Dense, LeakyReLU
+from keras.layers import LSTM, Dense
 from keras.optimizers import Adam
 from matplotlib import pyplot as plt
 from topoml_util.ConsoleLogger import DecypherAll
@@ -13,7 +13,7 @@ from topoml_util.gaussian_loss import univariate_gaussian_loss
 from topoml_util.geom_scaler import localized_normal, localized_mean
 from topoml_util.slack_send import notify
 
-SCRIPT_VERSION = "0.1.2"
+SCRIPT_VERSION = "0.1.3"
 SCRIPT_NAME = os.path.basename(__file__)
 TIMESTAMP = str(datetime.now()).replace(':', '.')
 SIGNATURE = SCRIPT_NAME + ' ' + TIMESTAMP
@@ -51,9 +51,24 @@ for inputs, targets in zip(raw_input_vectors, raw_target_vectors):
         target_vectors.append(targets)
         intersecting_count += 1
 
+input_vectors = np.array(input_vectors)
+target_vectors = np.array(target_vectors)
+
+# plot distance distribution
+mu = np.mean(target_vectors[:, 0])
+sigma = np.std(target_vectors[:, 0])
+fig, ax = plt.subplots()
+plt.text(0.70, 0.94, r'distance $\mu: $' + str(np.round(mu, 4)), transform=ax.transAxes)
+plt.text(0.70, 0.88, r'distance $\sigma: $' + str(np.round(sigma, 4)), transform=ax.transAxes)
+plt.xlabel('Distance')
+plt.ylabel('Frequency')
+plt.title('Geometric distance distribution in meters')
+n, bins, patches = plt.hist(target_vectors[:, 0], 50, facecolor='g', normed=False, log=True, alpha=0.75)
+os.makedirs(str(PLOT_DIR), exist_ok=True)
+plt.savefig(PLOT_DIR + '/plt_' + SIGNATURE + '_distance_distr.png')
+
 inputs = Input(name='Input', shape=(max_points, GEO_VECTOR_LEN))
-model = LSTM(LATENT_SIZE)(inputs)
-model = LeakyReLU()(model)
+model = LSTM(LATENT_SIZE, activation='relu')(inputs)
 model = Dense(2, activation='relu')(model)
 model = Model(inputs, model)
 model.compile(
@@ -78,13 +93,13 @@ history = model.fit(
 val_set_start = -round(data_points * TRAIN_VALIDATE_SPLIT)
 prediction = model.predict(raw_input_vectors[val_set_start:])
 error = prediction[:, 0] - raw_target_vectors[val_set_start:, 0]
-fig, ax = plt.subplots()
+_, ax = plt.subplots()
 plt.text(0.01, 0.94, r'prediction error $\mu: $' + str(np.round(np.mean(error), 4)), transform=ax.transAxes)
 plt.text(0.01, 0.88, r'prediction error $\sigma: $' + str(np.round(np.std(error), 4)), transform=ax.transAxes)
 plt.xlabel('Error')
 plt.ylabel('Frequency')
-plt.title('Geometric distance error')
-n, bins, patches = plt.hist(error, 50, facecolor='g', normed=False, alpha=0.75)
+plt.title('Geometric distance error distribution in meters')
+plt.hist(error, 50, facecolor='g', normed=False, alpha=0.75)
 os.makedirs(str(PLOT_DIR), exist_ok=True)
 plt.savefig(PLOT_DIR + '/plt_' + SIGNATURE + '.png')
 
