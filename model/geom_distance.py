845cf969f60095598dd15cee134e8026a4bf8f89
reinvantveer <rein.van.t.veer@geodan.nl>
2017-11-13 10:56:38 +0100

    bump stacked setup

--- model/geom_distance.py
+++ model/geom_distance.py
@@ -13,7 +13,7 @@ from topoml_util.gaussian_loss import univariate_gaussian_loss
 from topoml_util.geom_scaler import localized_normal, localized_mean
 from topoml_util.slack_send import notify
 
-SCRIPT_VERSION = "0.1.6"
+SCRIPT_VERSION = "0.1.7"
 SCRIPT_NAME = os.path.basename(__file__)
 TIMESTAMP = str(datetime.now()).replace(':', '.')
 SIGNATURE = SCRIPT_NAME + ' ' + TIMESTAMP
