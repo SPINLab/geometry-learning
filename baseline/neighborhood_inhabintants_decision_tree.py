5088feffc17d8c001c35e8ff3f1525a6c9ffacd6
reinvantveer <rein.van.t.veer@geodan.nl>
2017-12-14 17:08:21 +0100

    fix target values, test results

--- baseline/neighborhood_inhabintants_decision_tree.py
+++ baseline/neighborhood_inhabintants_decision_tree.py
@@ -25,26 +25,30 @@ NUM_CPUS = multiprocessing.cpu_count() - 1 if multiprocessing.cpu_count() > 1 el
 if __name__ == '__main__':  # this is to squelch warnings on scikit-learn multithreaded grid search
     train_loaded = np.load(TRAINING_DATA_FILE)
     train_fourier_descriptors = train_loaded['fourier_descriptors']
-    train_above_or_below_median = train_loaded['above_or_below_median'][:, 0]
-    train_above_or_below_median = np.reshape(train_above_or_below_median, (train_above_or_below_median.shape[0]))
+    train_above_or_below_median = train_loaded['above_or_below_median']
 
     scaler = StandardScaler().fit(train_fourier_descriptors)
     train_fourier_descriptors = scaler.transform(train_fourier_descriptors)
     clf = tree.DecisionTreeClassifier()
-    cross_val_score(clf, train_fourier_descriptors, train_fourier_descriptors, cv=10, n_jobs=NUM_CPUS)
+
+    print('Fitting data to model...')
+    scores = cross_val_score(clf, train_fourier_descriptors, train_above_or_below_median, cv=10, n_jobs=NUM_CPUS)
+    print('Cross-validation scores:', scores)
+    clf.fit(train_fourier_descriptors, train_above_or_below_median)
 
     # Run predictions on unseen test data to verify generalization
     TEST_DATA_FILE = '../files/neighborhoods/neighborhoods_test.npz'
     test_loaded = np.load(TEST_DATA_FILE)
     test_fourier_descriptors = test_loaded['fourier_descriptors']
-    test_above_or_below_median = test_loaded['above_or_below_median'][:, 0]
-    test_above_or_below_median = np.reshape(test_above_or_below_median, (test_above_or_below_median.shape[0]))
+    test_above_or_below_median = np.asarray(test_loaded['above_or_below_median'], dtype=int)
     test_fourier_descriptors = scaler.transform(test_fourier_descriptors)
+
+    print('Run on test data...')
     predictions = clf.predict(test_fourier_descriptors)
 
     correct = 0
     for prediction, expected in zip(predictions, test_above_or_below_median):
-        if prediction == expected:
+        if all([pred == exp for pred, exp in zip(prediction, expected)]):
             correct += 1
 
     accuracy = correct / len(predictions)
