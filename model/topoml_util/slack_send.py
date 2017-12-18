e403c72975a87e757b37203522f656c5bcf1e772
reinvantveer <rein.van.t.veer@geodan.nl>
2017-12-18 12:48:27 +0100

    refactor

--- model/topoml_util/slack_send.py
+++ model/topoml_util/slack_send.py
@@ -4,12 +4,12 @@ from slackclient import SlackClient
 slack_token = os.environ.get("SLACK_API_TOKEN")
 
 
-def notify(timestamp, script_name, message):
+def notify(signature, message):
     if slack_token:
         sc = SlackClient(slack_token)
         sc.api_call(
           "chat.postMessage",
           channel="#machinelearning",
-          text="Session " + timestamp + ' ' + script_name + " completed successfully with: " + str(message))
+          text="Session " + signature + " completed successfully with: " + str(message))
     else:
         print('No slack notification: no slack API token environment variable "SLACK_API_TOKEN" set.')
