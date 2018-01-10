7a43c5039f8b92291b59d29e947f12fd7f30522f
reinvantveer <rein.van.t.veer@geodan.nl>
2018-01-10 09:25:01 +0100

    update data and preprocessing

--- prep/preprocess-buildings.py
+++ prep/preprocess-buildings.py
@@ -21,7 +21,7 @@ TRAIN_TEST_SPLIT = 0.1
 FOURIER_DESCRIPTOR_ORDER = 16  # The axis 0 size
 TRAIN_DATA_FILE = '../files/buildings/buildings-train-'
 TEST_DATA_FILE = '../files/buildings/buildings-test.npz'
-NUMBER_OF_FILES = 6
+NUMBER_OF_FILES = 4
 
 building_types = [
     'bijeenkomstfunctie',  # gatherings
@@ -52,7 +52,7 @@ for builing_type in building_types:
     print('Processing', path)
 
     if not os.path.isfile(path):
-        raise FileNotFoundError('Unable to locate %s. Please run the get-data.sh script first' % path)
+        raise FileNotFoundError('Unable to locate %s. Please run the prep/get-data.sh script first' % path)
 
     with open(path) as data_file:
         df = read_csv(data_file)
@@ -63,15 +63,12 @@ for builing_type in building_types:
         try:
             geoms.append(GeoVectorizer.vectorize_wkt(wkt_string, SANE_NUMBER_OF_POINTS, simplify=True))
         except Exception as e:
-            raise ValueError('Incorrect geometry entry in {0} on line {1}: {2} with error {3}'.format(
-                path,
-                index + 2,
-                wkt_string,
-                e))
+            raise ValueError('Incorrect geometry entry in {0} on line {1}: {2} with error {3}'
+                             .format(path, index + 2, wkt_string, e))
 
     print('Creating building geometry fourier descriptors...')
     shapes = []
-    for wkt_string in df.geometrie.values:
+    for wkt_string in df.geometrie.values:  # create the descriptors on the untruncated geoms
         shape = wkt.loads(wkt_string)
         # If multipart multipolygon: select the largest, but it will throw off the accuracy a bit.
         if shape.geom_type == 'MultiPolygon':
@@ -89,41 +86,52 @@ for builing_type in building_types:
     # Labels
     type_int = [building_types.index(building_type) for building_type in df.gebruiksdoel]
 
-    training_data['geoms'].append(geoms[:-train_test_split_index])
-    training_data['fourier_descriptors'].append(fourier_descriptors[:-train_test_split_index])
-    training_data['building_type'].append(type_int[:-train_test_split_index])
+    if len(training_data['geoms']) == 0:
+        training_data['geoms'] = geoms[:-train_test_split_index]
+        training_data['fourier_descriptors'] = fourier_descriptors[:-train_test_split_index]
+        training_data['building_type'] = type_int[:-train_test_split_index]
 
-    test_data['geoms'].append(geoms[-train_test_split_index:])
-    test_data['fourier_descriptors'].append(fourier_descriptors[-train_test_split_index:])
-    test_data['building_type'].append(type_int[-train_test_split_index:])
+        test_data['geoms'] = geoms[-train_test_split_index:]
+        test_data['fourier_descriptors'] = fourier_descriptors[-train_test_split_index:]
+        test_data['building_type'] = type_int[-train_test_split_index:]
+    else:
+        training_data['geoms'] = \
+            np.append(training_data['geoms'], geoms[:-train_test_split_index], axis=0)
+        training_data['fourier_descriptors'] = \
+            np.append(training_data['fourier_descriptors'], fourier_descriptors[:-train_test_split_index], axis=0)
+        training_data['building_type'] = \
+            np.append(training_data['building_type'], type_int[:-train_test_split_index], axis=0)
+
+        test_data['geoms'] = \
+            np.append(test_data['geoms'], geoms[-train_test_split_index:], axis=0)
+        test_data['fourier_descriptors'] = \
+            np.append(test_data['fourier_descriptors'], fourier_descriptors[-train_test_split_index:], axis=0)
+        test_data['building_type'] = \
+            np.append(test_data['building_type'], type_int[-train_test_split_index:], axis=0)
 
 print('Saving training and test data files...')
 
-part_size = round(len(training_data['geoms']) / NUMBER_OF_FILES)
+# Create nicely even-sized chunks
+for offset in range(NUMBER_OF_FILES):
+    stride = NUMBER_OF_FILES  # just an alias
 
-# Create git-sized chunks
-for part in range(NUMBER_OF_FILES):
-    start_index = part_size * part
-    stop_index = part_size * (part + 1)
-    section = {
-        'geoms': training_data['geoms'][start_index:stop_index],
-        'fourier_descriptors': training_data['fourier_descriptors'][start_index:stop_index],
-        'building_type': training_data['building_type'][start_index:stop_index]
-    }
+    part_geoms = training_data['geoms'][offset::stride]
+    part_fourier_descriptors = training_data['fourier_descriptors'][offset::stride]
+    part_building_type = training_data['building_type'][offset::stride]
 
     np.savez_compressed(
-        TRAIN_DATA_FILE + str(part),
-        GEOMS=section['geoms'],
-        FOURIER_DESCRIPTORS=section['fourier_descriptors'],
-        BUILDING_TYPE=section['building_type'],
+        TRAIN_DATA_FILE + str(offset),
+        geoms=part_geoms,
+        fourier_descriptors=part_fourier_descriptors,
+        building_type=part_building_type,
     )
 
 # Test data is small enough to put in one archive
 np.savez_compressed(
     TEST_DATA_FILE,
-    GEOMS=test_data['geoms'],
-    FOURIER_DESCRIPTORS=test_data['fourier_descriptors'],
-    BUILDING_TYPE=test_data['building_type'],
+    geoms=test_data['geoms'],
+    fourier_descriptors=test_data['fourier_descriptors'],
+    building_type=test_data['building_type'],
 )
 
 print('Done!')
