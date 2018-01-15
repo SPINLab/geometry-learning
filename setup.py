8327449b31129df8d5503663274ead7d7db3d659
reinvantveer <rein.van.t.veer@geodan.nl>
2018-01-15 12:54:27 +0100

    deps

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Topology learning',
+      version='1.0',
+      description='Machine learning experiments for geospatial vector geometries',
+      author='Rein van \'t Veer',
+      author_email='rein.van.t.veer@geodan.nl',
+      url='https://github.com/reinvantveer/Topology-Learning',
+      packages=['model', 'model.topoml_util', 'model.baseline'],
+      license='MIT',
+      install_requires=[
+          'sklearn',
+          'slackclient',
+          'scipy',
+          'keras',
+          'numpy',
+          'shapely'
+      ],
+      )
+
