7759ff309ea969254e69c50f239dd478405f38d2
reinvantveer <rein.van.t.veer@geodan.nl>
2018-01-15 16:38:30 +0100

    Add softmax, slightly larger LSTM

--- model/neighborhood_inhabitants.py
+++ model/neighborhood_inhabitants.py
@@ -11,7 +11,7 @@ from keras.optimizers import Adam
 from topoml_util.geom_scaler import localized_mean, localized_normal
 from topoml_util.slack_send import notify
 
-SCRIPT_VERSION = '0.0.6'
+SCRIPT_VERSION = '0.0.7'
 SCRIPT_NAME = os.path.basename(__file__)
 TIMESTAMP = str(datetime.now()).replace(':', '.')
 SIGNATURE = SCRIPT_NAME + ' ' + TIMESTAMP
@@ -20,8 +20,8 @@ TRAINING_DATA_FILE = '../files/neighborhoods/neighborhoods_train.npz'
 # Hyperparameters
 BATCH_SIZE = int(os.getenv('BATCH_SIZE', 32))
 TRAIN_VALIDATE_SPLIT = float(os.getenv('TRAIN_VALIDATE_SPLIT', 0.1))
-REPEAT_DEEP_ARCH = int(os.getenv('REPEAT_DEEP_ARCH', 0))
-LSTM_SIZE = int(os.getenv('LSTM_SIZE', 128))
+REPEAT_DEEP_ARCH = int(os.getenv('REPEAT_DEEP_ARCH', 1))
+LSTM_SIZE = int(os.getenv('LSTM_SIZE', 192))
 DENSE_SIZE = int(os.getenv('DENSE_SIZE', 32))
 EPOCHS = int(os.getenv('EPOCHS', 400))
 LEARNING_RATE = float(os.getenv('LEARNING_RATE', 1e-4))
@@ -62,13 +62,13 @@ output_seq_length = train_above_or_below_median.shape[-1]
 
 # Build model
 inputs = Input(shape=(geom_max_points, geom_vector_len))
-model = LSTM(LSTM_SIZE, activation='relu', return_sequences=True, recurrent_dropout=0.1)(inputs)
+model = LSTM(LSTM_SIZE, activation='relu', return_sequences=True)(inputs)
 
 for layer in range(REPEAT_DEEP_ARCH):
-    model = LSTM(LSTM_SIZE, activation='relu', return_sequences=True)(model)
+    model = LSTM(LSTM_SIZE, activation='relu')(model)
 
-model = TimeDistributed(Dense(DENSE_SIZE, activation='relu'))(model)
-model = Flatten()(model)
+# model = TimeDistributed(Dense(DENSE_SIZE, activation='relu'))(model)
+model = Dense(DENSE_SIZE, activation='softmax')(model)
 model = Dense(output_seq_length)(model)
 
 model = Model(inputs=inputs, outputs=model)
