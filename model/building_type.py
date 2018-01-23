373e6f683816fb33395a04ef09b88b6edbdeba73
reinvantveer <rein.van.t.veer@geodan.nl>
2018-01-23 13:45:20 +0100

    3 LSTM with dropout

--- model/building_type.py
+++ model/building_type.py
@@ -16,7 +16,7 @@ from keras.optimizers import Adam
 from topoml_util.geom_scaler import localized_mean, localized_normal
 from topoml_util.slack_send import notify
 
-SCRIPT_VERSION = '0.0.14'
+SCRIPT_VERSION = '0.0.15'
 SCRIPT_NAME = os.path.basename(__file__)
 TIMESTAMP = str(datetime.now()).replace(':', '.')
 SIGNATURE = SCRIPT_NAME + ' ' + TIMESTAMP
@@ -26,7 +26,7 @@ FILENAME_PREFIX = 'buildings-train'
 # Hyperparameters
 BATCH_SIZE = int(os.getenv('BATCH_SIZE', 256))
 TRAIN_VALIDATE_SPLIT = float(os.getenv('TRAIN_VALIDATE_SPLIT', 0.1))
-REPEAT_DEEP_ARCH = int(os.getenv('REPEAT_DEEP_ARCH', 1))
+REPEAT_DEEP_ARCH = int(os.getenv('REPEAT_DEEP_ARCH', 2))
 LSTM_SIZE = int(os.getenv('LSTM_SIZE', 256))
 DENSE_SIZE = int(os.getenv('DENSE_SIZE', 64))
 EPOCHS = int(os.getenv('EPOCHS', 400))
