ae270d03781cb17171369153e6fdc7d9ab7b5625
reinvantveer <rein.van.t.veer@geodan.nl>
2018-01-24 07:10:21 +0100

    Retry with best known settings

--- model/building_type.py
+++ model/building_type.py
@@ -16,7 +16,7 @@ from keras.optimizers import Adam
 from topoml_util.geom_scaler import localized_mean, localized_normal
 from topoml_util.slack_send import notify
 
-SCRIPT_VERSION = '0.0.16'
+SCRIPT_VERSION = '0.0.17'
 SCRIPT_NAME = os.path.basename(__file__)
 TIMESTAMP = str(datetime.now()).replace(':', '.')
 SIGNATURE = SCRIPT_NAME + ' ' + TIMESTAMP
@@ -29,7 +29,7 @@ TRAIN_VALIDATE_SPLIT = float(os.getenv('TRAIN_VALIDATE_SPLIT', 0.1))
 REPEAT_DEEP_ARCH = int(os.getenv('REPEAT_DEEP_ARCH', 1))
 LSTM_SIZE = int(os.getenv('LSTM_SIZE', 256))
 DENSE_SIZE = int(os.getenv('DENSE_SIZE', 64))
-EPOCHS = int(os.getenv('EPOCHS', 400))
+EPOCHS = int(os.getenv('EPOCHS', 44))
 LEARNING_RATE = float(os.getenv('LEARNING_RATE', 1e-3))
 GEOM_SCALE = int(os.getenv('GEOM_SCALE', 0))  # Default 0, overridden when data is known
 OPTIMIZER = Adam(lr=LEARNING_RATE)
@@ -88,7 +88,7 @@ model = LSTM(LSTM_SIZE, activation='relu', return_sequences=True, recurrent_drop
 
 for layer in range(REPEAT_DEEP_ARCH):
     model = LSTM(LSTM_SIZE, activation='relu', return_sequences=True, recurrent_dropout=0.1)(model)
-    model = TimeDistributed(Dense(DENSE_SIZE, activation='relu'))(model)
+    # model = TimeDistributed(Dense(DENSE_SIZE, activation='relu'))(model)
 
 model = Dense(DENSE_SIZE, activation='relu')(model)
 model = Flatten()(model)
