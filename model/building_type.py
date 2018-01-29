f5645f967c7f06b7c2859cbf944c8642821aacf5
reinvantveer <rein.van.t.veer@geodan.nl>
2018-01-29 12:53:25 +0100

    Tweak

--- model/building_type.py
+++ model/building_type.py
@@ -16,7 +16,7 @@ from keras.optimizers import Adam
 from topoml_util.geom_scaler import localized_mean, localized_normal
 from topoml_util.slack_send import notify
 
-SCRIPT_VERSION = '0.1.22'
+SCRIPT_VERSION = '0.1.23'
 SCRIPT_NAME = os.path.basename(__file__)
 TIMESTAMP = str(datetime.now()).replace(':', '.')
 SIGNATURE = SCRIPT_NAME + ' ' + TIMESTAMP
@@ -26,10 +26,10 @@ FILENAME_PREFIX = 'buildings-train'
 # Hyperparameters
 BATCH_SIZE = int(os.getenv('BATCH_SIZE', 384))
 TRAIN_VALIDATE_SPLIT = float(os.getenv('TRAIN_VALIDATE_SPLIT', 0.1))
-REPEAT_DEEP_ARCH = int(os.getenv('REPEAT_DEEP_ARCH', 2))
+REPEAT_DEEP_ARCH = int(os.getenv('REPEAT_DEEP_ARCH', 0))
 LSTM_SIZE = int(os.getenv('LSTM_SIZE', 256))
 DENSE_SIZE = int(os.getenv('DENSE_SIZE', 64))
-EPOCHS = int(os.getenv('EPOCHS', 100))
+EPOCHS = int(os.getenv('EPOCHS', 200))
 LEARNING_RATE = float(os.getenv('LEARNING_RATE', 3e-4))
 GEOM_SCALE = int(os.getenv('GEOM_SCALE', 0))  # Default 0, overridden when data is known
 OPTIMIZER = Adam(lr=LEARNING_RATE)
