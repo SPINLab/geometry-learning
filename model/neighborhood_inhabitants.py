a353804ec14e5c0c707ed742c7c3a4a02711b81e
reinvantveer <rein.van.t.veer@geodan.nl>
2018-01-31 16:10:05 +0100

    lower learning rate to 3e-5

--- model/neighborhood_inhabitants.py
+++ model/neighborhood_inhabitants.py
@@ -11,7 +11,7 @@ from keras.optimizers import Adam
 from topoml_util.geom_scaler import localized_mean, localized_normal
 from topoml_util.slack_send import notify
 
-SCRIPT_VERSION = '0.0.17'
+SCRIPT_VERSION = '0.0.18'
 SCRIPT_NAME = os.path.basename(__file__)
 TIMESTAMP = str(datetime.now()).replace(':', '.')
 SIGNATURE = SCRIPT_NAME + ' ' + TIMESTAMP
@@ -24,7 +24,7 @@ REPEAT_DEEP_ARCH = int(os.getenv('REPEAT_DEEP_ARCH', 1))
 LSTM_SIZE = int(os.getenv('LSTM_SIZE', 256))
 DENSE_SIZE = int(os.getenv('DENSE_SIZE', 64))
 EPOCHS = int(os.getenv('EPOCHS', 200))
-LEARNING_RATE = float(os.getenv('LEARNING_RATE', 1e-4))
+LEARNING_RATE = float(os.getenv('LEARNING_RATE', 3e-5))
 PATIENCE = 40
 RECURRENT_DROPOUT = 0.05
 GEOM_SCALE = int(os.getenv('GEOM_SCALE', 0))  # Default 0, overridden when data is known
