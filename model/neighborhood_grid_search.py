83ea58d99dcadb9538ba892b5331bfad8e5bedd1
reinvantveer <rein.van.t.veer@geodan.nl>
2018-02-02 17:27:50 +0100

    some more parameter searching with dropout settings

--- model/neighborhood_grid_search.py
+++ model/neighborhood_grid_search.py
@@ -7,11 +7,13 @@ SCRIPT_VERSION = '0.0.7'
 HYPERPARAMS = {
     # 'BATCH_SIZE': [512],
     'REPEAT_DEEP_ARCH': [0, 1],
-    # 'LSTM_SIZE': [256],
+    'LSTM_SIZE': [192, 256, 384],
     # 'DENSE_SIZE': [64],
     # 'EPOCHS': [200],
     'LEARNING_RATE': [1e-3, 3e-4, 1e-4],
-    'GEOM_SCALE': [1e0, 1e-1, 1e-2, 1e-3]
+    # 'GEOM_SCALE': [1e0, 1e-1, 1e-2, 1e-3],
+    'RECURRENT_DROPOUT': [0.0, 0.05, 0.1]
+
 }
 grid = list(ParameterGrid(HYPERPARAMS))
 
