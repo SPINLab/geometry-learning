4d10346cc5bfc0575cff6df0aeea98604191c86d
reinvantveer <rein.van.t.veer@geodan.nl>
2018-02-02 13:34:51 +0100

    file names, skip records with faulty geoms

--- prep/preprocess-archaeology.py
+++ prep/preprocess-archaeology.py
@@ -1,15 +1,15 @@
 import os
 
 import collections
-from model.topoml_util.geom_fourier_descriptors import geom_fourier_descriptors
+from model.topoml_util.geom_fourier_descriptors import geom_fourier_descriptors, create_geom_fourier_descriptor
 from model.topoml_util.GeoVectorizer import GeoVectorizer
 from pandas import read_csv
 from shapely import wkt
 import numpy as np
 
 SOURCE = '../files/archaeology/combined_arch_feat.csv'
-TRAIN_DATA_FILE = '../files/archaeology/archeo_features_train.npz'
-TEST_DATA_FILE = '../files/archaeology/archeo_features_test.npz'
+TRAIN_DATA_FILE = '../files/archaeology/archaeo_features_train.npz'
+TEST_DATA_FILE = '../files/archaeology/archaeo_features_test.npz'
 SANE_NUMBER_OF_POINTS = 256
 TRAIN_TEST_SPLIT = 0.1
 FOURIER_DESCRIPTOR_ORDER = 16  # The axis 0 size
@@ -32,48 +32,57 @@ included_classes = [f_type for f_type, count in class_count.items() if count > M
 print('Included classes:', included_classes)
 
 # geometry vectors
-print('Creating geometry vectors...')
+print('Creating geometry vectors and descriptors...')
 feature_types = []
-wkts = []
+shapes = []  # shapely shapes
 wkt_vectors = []
+fourier_descriptors = []
+errors = 0
+
 for index, (feature, geom) in enumerate(zip(aardspoor__as_matrix, wkt__as_matrix)):
     if feature in included_classes:
         try:
-            wkt_vectors.append(
-                GeoVectorizer.vectorize_wkt(geom, SANE_NUMBER_OF_POINTS, simplify=True))
-            wkts.append(geom)
+            wkt_vector = GeoVectorizer.vectorize_wkt(geom, SANE_NUMBER_OF_POINTS, simplify=True)
+
+            # create the descriptors on the untruncated geoms
+            shape = wkt.loads(geom)
+            # If multipart multipolygon: reduce to polygon by selecting the largest,
+            # but it will throw off the accuracy a bit.
+            if shape.geom_type == 'MultiPolygon':
+                if len(shape.geoms) > 1:
+                    geometries = sorted(shape.geoms, key=lambda x: x.area)
+                    shape = geometries[-1]
+                else:
+                    shape = shape.geoms[0]
+                fourier_descriptor = create_geom_fourier_descriptor(shape, FOURIER_DESCRIPTOR_ORDER)
+            elif shape.geom_type == 'Polygon':
+                fourier_descriptor = create_geom_fourier_descriptor(shape, FOURIER_DESCRIPTOR_ORDER)
+            else:
+                print('skipping record: no (multi)polygon entry in {0} on line {1}'.format(
+                    SOURCE, index + 2))
+                errors += 1
+                continue
+
         except Exception as e:
             print('skipping record on account of geometry entry in {0} on line {1} with error: {2}'.format(
                 SOURCE, index + 2, e))
+            errors += 1
             continue
-        feature_types.append(feature)
-        wkts.append(geom)
+
+        # Append the converted values if all went well
+        wkt_vectors.append(wkt_vector)
+        fourier_descriptors.append(fourier_descriptor)
+        # Convert types to numerical indices
+        feature_types.append(included_classes.index(feature))
+
+print('created {0} data points with {1} errors'.format(len(wkt_vectors), errors))
 
 class_distr = collections.Counter(feature_types)
 print('data points:', len(feature_types), 'over', len(dict(class_distr)), 'classes')
 print('class distribution:', class_distr)
 
-# Convert types to indices
-feature_types = [included_classes.index(f_type) for f_type in feature_types]
-
-print('Creating geometry fourier descriptors...')
-shapes = []
-for wkt_string in wkts:  # create the descriptors on the untruncated geoms
-    shape = wkt.loads(wkt_string)
-    # If multipart multipolygon: select the largest, but it will throw off the accuracy a bit.
-    if shape.geom_type == 'MultiPolygon':
-        if len(shape.geoms) > 1:
-            geometries = sorted(shape.geoms, key=lambda x: x.area)
-            shapes.append(geometries[-1])
-        else:
-            shapes.append(shape.geoms[0])
-    else:
-        shapes.append(shape)
-
-fourier_descriptors = geom_fourier_descriptors(shapes, FOURIER_DESCRIPTOR_ORDER)
-
 # Split and save data
-train_test_split_index = round(TRAIN_TEST_SPLIT * len(wkts))
+train_test_split_index = round(TRAIN_TEST_SPLIT * len(feature_types))
 
 training_data = {
     'geoms': wkt_vectors[:-train_test_split_index],
