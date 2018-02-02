5a8c0683ad8c60930e6356f90211d19d07df6312
reinvantveer <rein.van.t.veer@geodan.nl>
2018-02-02 09:36:40 +0100

    try some different scale settings

--- model/neighborhood_grid_search.py
+++ model/neighborhood_grid_search.py
@@ -2,15 +2,16 @@ import os
 from sklearn.model_selection import ParameterGrid
 from topoml_util.slack_send import notify
 
-SCRIPT_VERSION = '0.0.5'
+SCRIPT_VERSION = '0.0.6'
 
 HYPERPARAMS = {
-    'BATCH_SIZE': [128],
-    'REPEAT_DEEP_ARCH': [1],
-    'LSTM_SIZE': [256],
-    'DENSE_SIZE': [64],
-    'EPOCHS': [20],
-    'LEARNING_RATE': [1e-4]
+    # 'BATCH_SIZE': [512],
+    # 'REPEAT_DEEP_ARCH': [0],
+    # 'LSTM_SIZE': [256],
+    # 'DENSE_SIZE': [64],
+    # 'EPOCHS': [200],
+    # 'LEARNING_RATE': [1e-4],
+    'GEOM_SCALE': [1e0, 1e-1, 1e-2, 1e-3]
 }
 grid = list(ParameterGrid(HYPERPARAMS))
 
