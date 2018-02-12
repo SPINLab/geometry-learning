35a5ec7172b26e5b9e5185797c8b364c233c92f7
reinvantveer <rein.van.t.veer@geodan.nl>
2018-02-12 17:16:49 +0100

    smaller batch, standard train on training data only

--- model/neighborhood_inhabitants.py
+++ model/neighborhood_inhabitants.py
@@ -14,7 +14,7 @@ from sklearn.model_selection import train_test_split
 from topoml_util import geom_scaler
 from topoml_util.slack_send import notify
 
-SCRIPT_VERSION = '1.2.34'
+SCRIPT_VERSION = '1.0.0'
 SCRIPT_NAME = os.path.basename(__file__)
 TIMESTAMP = str(datetime.now()).replace(':', '.')
 SIGNATURE = SCRIPT_NAME + ' ' + TIMESTAMP
