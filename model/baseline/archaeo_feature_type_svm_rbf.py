f71d3362f025e0ae4a2227cd4dd50afd4911b513
reinvantveer <rein.van.t.veer@geodan.nl>
2018-02-15 14:12:18 +0100

    refactor

--- model/baseline/archaeo_feature_type_svm_rbf.py
+++ model/baseline/archaeo_feature_type_svm_rbf.py
@@ -10,9 +10,11 @@ comparable.
 import os
 import sys
 import multiprocessing
-from datetime import datetime
+from time import time
+from datetime import datetime, timedelta
 
 import numpy as np
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import StratifiedShuffleSplit, GridSearchCV
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
@@ -23,11 +25,12 @@ sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 
 from topoml_util.slack_send import notify
 
-SCRIPT_VERSION = '0.0.1'
+SCRIPT_VERSION = '0.0.2'
 SCRIPT_NAME = os.path.basename(__file__)
 TIMESTAMP = str(datetime.now()).replace(':', '.')
 TRAINING_DATA_FILE = '../../files/archaeology/archaeo_features_train.npz'
 NUM_CPUS = multiprocessing.cpu_count() - 1 or 1
+SCRIPT_START = time()
 
 if __name__ == '__main__':  # this is to squelch warnings on scikit-learn multithreaded grid search
     # Load training data
@@ -38,23 +41,26 @@ if __name__ == '__main__':  # this is to squelch warnings on scikit-learn multit
     scaler = StandardScaler().fit(train_fourier_descriptors)
     train_fourier_descriptors = scaler.transform(train_fourier_descriptors)
 
-    C_range = [1e-2, 1e-1, 1e0, 1e1, 1e2, 1e3]
-    gamma_range = np.logspace(-3, 3, 7)
+    C_range = [1e-1, 1e0, 1e1, 1e2, 1e3]
+    gamma_range = np.logspace(-5, 3, 9)
     param_grid = dict(gamma=gamma_range, C=C_range)
     cv = StratifiedShuffleSplit(n_splits=5, test_size=0.2, random_state=42)
     grid = GridSearchCV(
-        SVC(kernel='rbf', verbose=True),
-        n_jobs=NUM_CPUS, param_grid=param_grid, cv=cv)
+        SVC(kernel='rbf', max_iter=int(1e7)),
+        n_jobs=NUM_CPUS,
+        param_grid=param_grid,
+        verbose=10,
+        cv=cv)
 
     print('Performing grid search on model...')
     print('Using %i threads for grid search' % NUM_CPUS)
-    grid.fit(X=train_fourier_descriptors, y=train_feature_type)
+    grid.fit(X=train_fourier_descriptors[::3], y=train_feature_type[::3])
 
     print("The best parameters are %s with a score of %0.3f"
           % (grid.best_params_, grid.best_score_))
 
     print('Training model on best parameters...')
-    clf = SVC(kernel='rbf', C=grid.best_params_['C'], gamma=grid.best_params_['gamma'], verbose=True)
+    clf = SVC(kernel='rbf', C=grid.best_params_['C'], gamma=grid.best_params_['gamma'])
     clf.fit(X=train_fourier_descriptors, y=train_feature_type)
 
     # Run predictions on unseen test data to verify generalization
@@ -66,21 +72,11 @@ if __name__ == '__main__':  # this is to squelch warnings on scikit-learn multit
     test_fourier_descriptors = scaler.transform(test_fourier_descriptors)
 
     predictions = clf.predict(test_fourier_descriptors)
-
-    correct = 0
-    for prediction, expected in zip(predictions, test_feature_type):
-        if prediction == expected:
-            correct += 1
-
-    accuracy = correct / len(predictions)
+    accuracy = accuracy_score(test_feature_type, predictions)
     print('Test accuracy: %0.3f' % accuracy)
 
-    message = 'test accuracy of {0} with ' \
-              'C: {1} ' \
-              'gamma: {2} ' \
-        .format(
-            str(accuracy),
-            grid.best_params_['C'],
-            grid.best_params_['gamma'])
+    runtime = time() - SCRIPT_START
+    message = 'test accuracy of {0} with C: {} gamma: {} in {}'.format(
+        str(accuracy), grid.best_params_['C'], grid.best_params_['gamma'], timedelta(seconds=runtime))
     notify(SCRIPT_NAME, message)
-    print(SCRIPT_NAME, 'finished successfully')
+    print(SCRIPT_NAME, 'finished successfully with', message)
