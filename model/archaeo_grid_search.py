e89624e1e116f42f1a0bdd9856064394f6b2709d
reinvantveer <rein.van.t.veer@geodan.nl>
2018-02-16 09:01:28 +0100

    bump

--- model/archaeo_grid_search.py
+++ model/archaeo_grid_search.py
@@ -4,7 +4,7 @@ import sys
 from sklearn.model_selection import ParameterGrid
 from topoml_util.slack_send import notify
 
-SCRIPT_VERSION = '0.0.2'
+SCRIPT_VERSION = '0.0.3'
 N_TIMES = 10
 
 HYPERPARAMS = {
