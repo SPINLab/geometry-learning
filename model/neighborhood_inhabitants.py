8ac94e744fb2f489b37509c0ab8f55dad8829da8
reinvantveer <rein.van.t.veer@geodan.nl>
2018-02-20 08:36:33 +0100

    dropout as on other tasks

--- model/neighborhood_inhabitants.py
+++ model/neighborhood_inhabitants.py
@@ -16,7 +16,7 @@ from sklearn.model_selection import train_test_split
 from topoml_util import geom_scaler
 from topoml_util.slack_send import notify
 
-SCRIPT_VERSION = '1.0.7'
+SCRIPT_VERSION = '1.0.8'
 SCRIPT_NAME = os.path.basename(__file__)
 TIMESTAMP = str(datetime.now()).replace(':', '.')
 SIGNATURE = SCRIPT_NAME + ' ' + TIMESTAMP
@@ -32,7 +32,7 @@ DENSE_SIZE = int(os.getenv('DENSE_SIZE', 32))
 EPOCHS = int(os.getenv('EPOCHS', 200))
 LEARNING_RATE = float(os.getenv('LEARNING_RATE', 1e-4))
 PATIENCE = int(os.getenv('PATIENCE', 16))
-RECURRENT_DROPOUT = float(os.getenv('RECURRENT_DROPOUT', 0.5))
+RECURRENT_DROPOUT = float(os.getenv('RECURRENT_DROPOUT', 0.05))
 GEOM_SCALE = float(os.getenv('GEOM_SCALE', 0))  # If no default or 0: overridden when data is known
 OPTIMIZER = Adam(lr=LEARNING_RATE, clipnorm=1.)
 
