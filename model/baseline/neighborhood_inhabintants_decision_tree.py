20ca322e045014e21310a171b47676d038ba7a62
reinvantveer <rein.van.t.veer@geodan.nl>
2018-03-22 13:53:46 +0100

    bump

--- model/baseline/neighborhood_inhabintants_decision_tree.py
+++ model/baseline/neighborhood_inhabintants_decision_tree.py
@@ -26,7 +26,7 @@ sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 
 from topoml_util.slack_send import notify
 
-SCRIPT_VERSION = '1.0.5'
+SCRIPT_VERSION = '1.0.6'
 SCRIPT_NAME = os.path.basename(__file__)
 TIMESTAMP = str(datetime.now()).replace(':', '.')
 TRAINING_DATA_FILE = '../../files/neighborhoods/neighborhoods_train.npz'
