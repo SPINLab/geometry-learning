043fb78278c3313281482aeec5e6d3c7282faad3
reinvantveer <rein.van.t.veer@geodan.nl>
2018-03-23 17:14:29 +0100

    bump

--- model/neighborhood_convnet.py
+++ model/neighborhood_convnet.py
@@ -16,7 +16,7 @@ from sklearn.model_selection import train_test_split
 from topoml_util import geom_scaler
 from topoml_util.slack_send import notify
 
-SCRIPT_VERSION = '0.0.2'
+SCRIPT_VERSION = '0.0.3'
 SCRIPT_NAME = os.path.basename(__file__)
 TIMESTAMP = str(datetime.now()).replace(':', '.')
 SIGNATURE = SCRIPT_NAME + ' ' + TIMESTAMP
