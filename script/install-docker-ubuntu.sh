9f08e7752cb6c5ce32d78b86abcb274ed28f75c3
reinvantveer <rein.van.t.veer@geodan.nl>
2018-03-23 15:31:02 +0100

    refactor dockerized build server setup

--- script/install-docker-ubuntu.sh
+++ script/install-docker-ubuntu.sh
@@ -12,5 +12,16 @@ sudo add-apt-repository \
    stable"
 sudo apt-get update
 sudo apt-get -y install docker-ce docker-compose
-sudo usermod -aG docker ${USER}
-echo "Please log out and in again to make use of the added user permissions on executing Docker commands"
+sudo gpasswd -a ${USER} docker
+newgrp docker
+
+# nvidia-docker
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | \
+  sudo apt-key add -
+distribution=$(. /etc/os-release;echo ${ID}${VERSION_ID})
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | \
+  sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+sudo apt-get update
+sudo apt-get install -y nvidia-docker2
+sudo pkill -SIGHUP dockerd
+sudo service docker restart
