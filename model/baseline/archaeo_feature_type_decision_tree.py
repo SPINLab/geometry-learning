c938434cbfe2923e80e95e3272367b546818bf19
reinvantveer <rein.van.t.veer@geodan.nl>
2018-04-10 10:16:12 +0200

    refactor to grid search fourier descriptor order

--- model/baseline/archaeo_feature_type_decision_tree.py
+++ model/baseline/archaeo_feature_type_decision_tree.py
@@ -26,7 +26,7 @@ sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 
 from topoml_util.slack_send import notify
 
-SCRIPT_VERSION = '1.0.0'
+SCRIPT_VERSION = '1.0.1'
 SCRIPT_NAME = os.path.basename(__file__)
 TIMESTAMP = str(datetime.now()).replace(':', '.')
 NUM_CPUS = multiprocessing.cpu_count() - 1 or 1
