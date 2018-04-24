457d875caca0d9d2dee83a955da729207097f7a1
reinvantveer <rein.van.t.veer@geodan.nl>
2018-04-24 14:56:33 +0200

    refactor to produce variable length geom vectors

--- prep/preprocess-archaeology.py
+++ prep/preprocess-archaeology.py
@@ -1,4 +1,5 @@
 import os
+import re
 from datetime import timedelta
 from time import time
 from zipfile import ZipFile
@@ -7,20 +8,24 @@ import collections
 import numpy as np
 from pandas import read_csv
 from shapely import wkt
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
 from model.topoml_util.GeoVectorizer import GeoVectorizer
 from model.topoml_util.geom_fourier_descriptors import create_geom_fourier_descriptor
 from prep.ProgressBar import ProgressBar
 
+SCRIPT_VERSION = '4'
 SOURCE_ZIP = '../files/archaeology/archaeology.csv.zip'
 SOURCE_CSV = 'archaeology.csv'
-TRAIN_DATA_FILE = '../files/archaeology/archaeology_order_30_train-'
-TEST_DATA_FILE = '../files/archaeology/archaeology_order_30_test.npz'
-SANE_NUMBER_OF_POINTS = 256
+LOG_FILE = 'archaeology_preprocessing.log'
+TRAIN_DATA_FILE = '../files/archaeology/archaeology_train_v{}-'.format(SCRIPT_VERSION)
+NUMBER_OF_FILES = 5
+TEST_DATA_FILE = '../files/archaeology/archaeology_test_v{}.npz'.format(SCRIPT_VERSION)
+SANE_NUMBER_OF_POINTS = 2048
 TRAIN_TEST_SPLIT = 0.1
-FOURIER_DESCRIPTOR_ORDER = 30  # The axis 0 size
+FOURIER_DESCRIPTOR_ORDER = 32  # The axis 0 size
 MINIMUM_CLASS_OCCURRENCE = 1000
-NUMBER_OF_FILES = 3
 SCRIPT_START = time()
 
 if not os.path.isfile(SOURCE_ZIP):
@@ -34,22 +39,37 @@ aardspoor__as_matrix = df['Aardspoor'].as_matrix()
 wkt__as_matrix = df['WKT'].as_matrix()
 
 class_count = dict(collections.Counter([f_type for f_type in aardspoor__as_matrix if type(f_type) == str]))
-# print('class count:', class_count)
 included_classes = [f_type for f_type, count in class_count.items() if count > MINIMUM_CLASS_OCCURRENCE]
 print('Included classes:', included_classes)
 
 # geometry vectors
 print('Creating geometry vectors and descriptors...')
 feature_types = []
-shapes = []  # shapely shapes
 wkt_vectors = []
 fourier_descriptors = []
 errors = 0
+shapes = []
+
+for wkt_string in df.WKT.values:
+    try:
+        shapes.append(wkt.loads(wkt_string))
+    except Exception as e:
+        print('Skipping unreadable wkt geom.')
+number_of_vertices = [len(re.findall('\d \d', shape.wkt)) for shape in shapes]
+
+plt.hist(number_of_vertices, bins=20, log=True)
+plt.savefig('archaeology_geom_vertices_distr.png')
+geoms_above_treshold = len([v for v in number_of_vertices if v > SANE_NUMBER_OF_POINTS])
+print('{} of the {} geometries marked as over the max {} vertices treshold will be simplified.\n'.format(
+    geoms_above_treshold, len(shapes), SANE_NUMBER_OF_POINTS))
 
 pgb = ProgressBar()
+logfile = open(LOG_FILE, 'w')
+
+selected_data = []
 
 for index, (feature, geom) in enumerate(zip(aardspoor__as_matrix, wkt__as_matrix)):
-    pgb.update_progress(index/len(aardspoor__as_matrix))
+    pgb.update_progress(index/len(aardspoor__as_matrix), '{} errors in logfile'.format(errors))
     if feature in included_classes:
         try:
             wkt_vector = GeoVectorizer.vectorize_wkt(geom, SANE_NUMBER_OF_POINTS, simplify=True)
@@ -64,9 +84,9 @@ for index, (feature, geom) in enumerate(zip(aardspoor__as_matrix, wkt__as_matrix
                     shape = geometries[-1]
                 else:
                     shape = shape.geoms[0]
-                fourier_descriptor = create_geom_fourier_descriptor(shape, FOURIER_DESCRIPTOR_ORDER)
+                fds = create_geom_fourier_descriptor(shape, FOURIER_DESCRIPTOR_ORDER)
             elif shape.geom_type == 'Polygon':
-                fourier_descriptor = create_geom_fourier_descriptor(shape, FOURIER_DESCRIPTOR_ORDER)
+                fds = create_geom_fourier_descriptor(shape, FOURIER_DESCRIPTOR_ORDER)
             else:
                 print('skipping record: no (multi)polygon entry in {0} on line {1}'.format(
                     SOURCE_CSV, index + 2))
@@ -74,83 +94,54 @@ for index, (feature, geom) in enumerate(zip(aardspoor__as_matrix, wkt__as_matrix
                 continue
 
         except Exception as e:
-            print('skipping record on account of geometry entry in {0} on line {1} with error: {2}'.format(
+            logfile.write('Skipping record on account of geometry entry in {0} on line {1} with error: {2}\n'.format(
                 SOURCE_CSV, index + 2, e))
             errors += 1
             continue
 
         # Append the converted values if all went well
-        wkt_vectors.append(wkt_vector)
-        fourier_descriptors.append(fourier_descriptor)
-        # Convert types to numerical indices
-        feature_types.append(included_classes.index(feature))
+        selected_data.append({
+                'geom': wkt_vector,
+                'fourier_descriptors': fds,
+                'feature_type': included_classes.index(feature), # Convert types to numerical index
+            })
 
-print('created {0} data points with {1} errors'.format(len(wkt_vectors), errors))
-
-class_distr = collections.Counter(feature_types)
-print('data points:', len(feature_types), 'over', len(dict(class_distr)), 'classes')
-print('class distribution:', class_distr)
+logfile.close()
+print('created {} data points with {} errors'.format(len(wkt_vectors), errors))
 
 # Split and save data
-n_parts = int(1 / TRAIN_TEST_SPLIT)
+train, test = train_test_split(selected_data, test_size=0.1, shuffle=False, random_state=42)
 
-wkt_vectors_parts = []
-fourier_descriptors_parts = []
-feature_types_parts = []
+print('Saving test data...')
+# Test data is small enough to put in one archive
+test_geoms = [record['geom'] for record in test]
+test_fds = [record['fourier_descriptors'] for record in test]
+test_fts = [record['feature_type'] for record in test]
+np.savez_compressed(
+    TEST_DATA_FILE,
+    geoms=test_geoms,
+    fourier_descriptors=test_fds,
+    feature_type=test_fts,
+    feature_type_index=included_classes)
 
-training_data = {
-    'geoms': [],
-    'fourier_descriptors': [],
-    'feature_type': [],
-    'feature_type_index': included_classes,
-}
+print('Saving training data...')
+train_geoms = [record['geom'] for record in train]
+train_fds = [record['fourier_descriptors'] for record in train]
+train_fts = [record['feature_type'] for record in train]
 
-test_data = {
-    'geoms': [],
-    'fourier_descriptors': [],
-    'feature_type': [],
-    'feature_type_index': included_classes,
-}
-
-for part in range(n_parts):
-    if part == 0:
-        test_data['geoms'] = wkt_vectors[part::n_parts]
-        test_data['fourier_descriptors'] = fourier_descriptors[part::n_parts]
-        test_data['feature_type'] = feature_types[part::n_parts]
-    elif part == 1:  # fill first part
-        training_data['geoms'] = wkt_vectors[part::n_parts]
-        training_data['fourier_descriptors'] = fourier_descriptors[part::n_parts]
-        training_data['feature_type'] = feature_types[part::n_parts]
-    else:  # append the rest
-        training_data['geoms'] = np.append(training_data['geoms'], wkt_vectors[part::n_parts], axis=0)
-        training_data['fourier_descriptors'] = np.append(training_data['fourier_descriptors'], fourier_descriptors[part::n_parts], axis=0)
-        training_data['feature_type'] = np.append(training_data['feature_type'], feature_types[part::n_parts], axis=0)
-
-print('Saving training and test data files...')
-# Create nicely even-sized chunks
+stride = NUMBER_OF_FILES  # just an alias
 for offset in range(NUMBER_OF_FILES):
-    stride = NUMBER_OF_FILES  # just an alias
-
-    part_geoms = training_data['geoms'][offset::stride]
-    part_fourier_descriptors = training_data['fourier_descriptors'][offset::stride]
-    part_feature_type = training_data['feature_type'][offset::stride]
+    print('Saving part {} of {}'.format(offset + 1, NUMBER_OF_FILES))
+    part_geoms = train_geoms[offset::stride]
+    part_fourier_descriptors = train_fds[offset::stride]
+    part_building_type = train_fts[offset::stride]
 
     np.savez_compressed(
         TRAIN_DATA_FILE + str(offset),
         geoms=part_geoms,
         fourier_descriptors=part_fourier_descriptors,
-        feature_type=part_feature_type,
-        feature_type_index=training_data['feature_type_index'],
-    )
-
-# Test data is small enough to put in one archive
-np.savez_compressed(
-    TEST_DATA_FILE,
-    geoms=test_data['geoms'],
-    fourier_descriptors=test_data['fourier_descriptors'],
-    feature_type=test_data['feature_type'],
-    feature_type_index=test_data['feature_type_index'],
-)
+        building_type=part_building_type,
+        feature_type_index=included_classes)
 
 runtime = time() - SCRIPT_START
 print('Done in {}'.format(timedelta(seconds=runtime)))
