a5060d3b0050eb81531c2e99fa11e3cb3e44c6fb
reinvantveer <rein.van.t.veer@geodan.nl>
2018-05-03 11:53:24 +0200

    LR sweep on buildings convnet

--- model/grid_search.py
+++ model/grid_search.py
@@ -16,10 +16,10 @@ if len(sys.argv) > 1:
     script_name = sys.argv[1]
 else:  # resort to default, for
     # script_name = 'neighborhood_dense.py'
-    script_name = 'neighborhood_convnet.py'
+    # script_name = 'neighborhood_convnet.py'
     # script_name = 'neighborhood_lstm.py'
     # script_name = 'building_dense.py'
-    # script_name = 'building_convnet.py'
+    script_name = 'building_convnet.py'
     # script_name = 'building_lstm.py'
     # script_name = 'archaeology_dense.py'
     # script_name = 'archaeology_convnet.py'
