9ead695a522181bcd4c77e8598fa7aab432b5703
reinvantveer <rein.van.t.veer@geodan.nl>
2018-05-10 13:07:41 +0200

    switch to dataverse, test batch size 256

--- model/building_lstm.py
+++ model/building_lstm.py
@@ -35,21 +35,21 @@ SCRIPT_NAME = os.path.basename(__file__)
 TIMESTAMP = str(datetime.now()).replace(':', '.')
 SIGNATURE = SCRIPT_NAME + ' ' + SCRIPT_VERSION + ' ' + TIMESTAMP
 DATA_FOLDER = '../files/buildings/'
-TRAIN_DATA_FILE = 'buildings_train_v5.npz'
-TEST_DATA_FILE = 'buildings_test_v5.npz'
-TRAIN_DATA_URL = 'https://surfdrive.surf.nl/files/index.php/s/owGX0srHn10iG6K/download'
-TEST_DATA_URL = 'https://surfdrive.surf.nl/files/index.php/s/LcGhwd3elb7EfE0/download'
+TRAIN_DATA_FILE = 'buildings_train_v6.npz'
+TEST_DATA_FILE = 'buildings_test_v6.npz'
+TRAIN_DATA_URL = 'https://dataverse.nl/api/access/datafile/11359'
+TEST_DATA_URL = 'https://dataverse.nl/api/access/datafile/11355'
 SCRIPT_START = time()
 
 # Hyperparameters
 hp = {
-    'BATCH_SIZE': int(os.getenv('BATCH_SIZE', 512)),
+    'BATCH_SIZE': int(os.getenv('BATCH_SIZE', 256)),
     'TRAIN_VALIDATE_SPLIT': float(os.getenv('TRAIN_VALIDATE_SPLIT', 0.1)),
     'REPEAT_DEEP_ARCH': int(os.getenv('REPEAT_DEEP_ARCH', 0)),
     'LSTM_SIZE': int(os.getenv('LSTM_SIZE', 32)),
     'DENSE_SIZE': int(os.getenv('DENSE_SIZE', 32)),
     'EPOCHS': int(os.getenv('EPOCHS', 200)),
-    'LEARNING_RATE': float(os.getenv('LEARNING_RATE', 4e-3)),
+    'LEARNING_RATE': float(os.getenv('LEARNING_RATE', 5e-4)),
     'RECURRENT_DROPOUT': float(os.getenv('RECURRENT_DROPOUT', 0.0)),
     'GEOM_SCALE': float(os.getenv("GEOM_SCALE", 0)),  # If no default or 0: overridden when data is known
 }
