3fd494b51f25e32e00790f57ada3c4d862fe4c67
reinvantveer <rein.van.t.veer@geodan.nl>
2018-05-16 13:28:19 +0200

    upgrade data: drop geometry type vector

--- model/baseline/building_type_knn.py
+++ model/baseline/building_type_knn.py
@@ -32,10 +32,10 @@ SCRIPT_NAME = os.path.basename(__file__)
 TIMESTAMP = str(datetime.now()).replace(':', '.')
 NUM_CPUS = multiprocessing.cpu_count() - 1 or 1
 DATA_FOLDER = SCRIPT_DIR + '/../../files/buildings/'
-TRAIN_DATA_FILE = 'buildings_train_v6.npz'
-TEST_DATA_FILE = 'buildings_test_v6.npz'
-TRAIN_DATA_URL = 'https://dataverse.nl/api/access/datafile/11359'
-TEST_DATA_URL = 'https://dataverse.nl/api/access/datafile/11355'
+TRAIN_DATA_FILE = 'buildings_train_v7.npz'
+TEST_DATA_FILE = 'buildings_test_v7.npz'
+TRAIN_DATA_URL = 'https://dataverse.nl/api/access/datafile/11381'
+TEST_DATA_URL = 'https://dataverse.nl/api/access/datafile/11380'
 EFD_ORDERS = [0, 1, 2, 3, 4, 6, 8, 12, 16, 20, 24]
 SCRIPT_START = time()
 
--- model/building_convnet.py
+++ model/building_convnet.py
@@ -30,25 +30,25 @@ from prep.ProgressBar import ProgressBar
 from topoml_util import geom_scaler
 from topoml_util.slack_send import notify
 
-SCRIPT_VERSION = '2.0.2'
+SCRIPT_VERSION = '2.0.3'
 SCRIPT_NAME = os.path.basename(__file__)
 TIMESTAMP = str(datetime.now()).replace(':', '.')
 SIGNATURE = SCRIPT_NAME + ' ' + SCRIPT_VERSION + ' ' + TIMESTAMP
 DATA_FOLDER = '../files/buildings/'
-TRAIN_DATA_FILE = 'buildings_train_v5.npz'
-TEST_DATA_FILE = 'buildings_test_v5.npz'
-TRAIN_DATA_URL = 'https://surfdrive.surf.nl/files/index.php/s/owGX0srHn10iG6K/download'
-TEST_DATA_URL = 'https://surfdrive.surf.nl/files/index.php/s/LcGhwd3elb7EfE0/download'
+TRAIN_DATA_FILE = 'buildings_train_v7.npz'
+TEST_DATA_FILE = 'buildings_test_v7.npz'
+TRAIN_DATA_URL = 'https://dataverse.nl/api/access/datafile/11381'
+TEST_DATA_URL = 'https://dataverse.nl/api/access/datafile/11380'
 SCRIPT_START = time()
 
 # Hyperparameters
 hp = {
-    'BATCH_SIZE': int(os.getenv('BATCH_SIZE', 512)),
+    'BATCH_SIZE': int(os.getenv('BATCH_SIZE', 32)),
     'TRAIN_VALIDATE_SPLIT': float(os.getenv('TRAIN_VALIDATE_SPLIT', 0.1)),
     'REPEAT_DEEP_ARCH': int(os.getenv('REPEAT_DEEP_ARCH', 0)),
     'DENSE_SIZE': int(os.getenv('DENSE_SIZE', 32)),
     'EPOCHS': int(os.getenv('EPOCHS', 200)),
-    'LEARNING_RATE': float(os.getenv('LEARNING_RATE', 4e-4)),
+    'LEARNING_RATE': float(os.getenv('LEARNING_RATE', 1e-4)),
     'DROPOUT': float(os.getenv('DROPOUT', 0.0)),
     'GEOM_SCALE': float(os.getenv("GEOM_SCALE", 0)),  # If no default or 0: overridden when data is known
 }
