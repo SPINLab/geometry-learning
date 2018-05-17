0666bf6745db5fdcdfb7f0cac786405475a37de7
reinvantveer <rein.van.t.veer@geodan.nl>
2018-05-17 13:25:58 +0200

    drop geometry type vector, add scale test

--- model/topoml_util/test_geom_scaler.py
+++ model/topoml_util/test_geom_scaler.py
@@ -5,42 +5,42 @@ import numpy as np
 from topoml_util import geom_scaler as gs
 
 # noinspection PyUnresolvedReferences
-dummy_geom = np.zeros((1, 1, 13))
+dummy_geom = np.zeros((1, 1, 5))
 
 square = np.array([[
-    [0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0.],
-    [1., 0., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0.],
-    [1., 1., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0.],
-    [0., 1., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0.],
-    [0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 1.],
+    [0., 0., 1., 0., 0.],
+    [1., 0., 1., 0., 0.],
+    [1., 1., 1., 0., 0.],
+    [0., 1., 1., 0., 0.],
+    [0., 0., 0., 0., 1.],
 ]])
 
 square_duplicate_nodes = np.array([[
-    [0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0.],
-    [1., 0., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0.],
-    [1., 1., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0.],
-    [1., 1., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0.],
-    [1., 1., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0.],
-    [1., 1., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0.],
-    [1., 1., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0.],
-    [0., 1., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0.],
-    [0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 1.],
+    [0., 0., 1., 0., 0.],
+    [1., 0., 1., 0., 0.],
+    [1., 1., 1., 0., 0.],
+    [1., 1., 1., 0., 0.],
+    [1., 1., 1., 0., 0.],
+    [1., 1., 1., 0., 0.],
+    [1., 1., 1., 0., 0.],
+    [0., 1., 1., 0., 0.],
+    [0., 0., 0., 0., 1.],
 ]])
 
 rectangle = np.array([[
-    [0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0.],
-    [1., 0., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0.],
-    [1., 2., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0.],
-    [0., 2., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0.],
-    [0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 1.],
+    [0., 0., 1., 0., 0.],
+    [1., 0., 1., 0., 0.],
+    [1., 2., 1., 0., 0.],
+    [0., 2., 1., 0., 0.],
+    [0., 0., 0., 0., 1.],
 ]])
 
 normalized_square = np.array([[
-    [-1., -1., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0.],
-    [ 1., -1., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0.],
-    [ 1.,  1., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0.],
-    [-1.,  1., 0., 0., 0., 0., 0., 0., 0., 0., 1., 0., 0.],
-    [-1., -1., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 1.],
+    [-1., -1., 1., 0., 0.],
+    [ 1., -1., 1., 0., 0.],
+    [ 1.,  1., 1., 0., 0.],
+    [-1.,  1., 1., 0., 0.],
+    [-1., -1., 0., 0., 1.],
 ]])
 
 
@@ -70,7 +70,22 @@ class TestGeomScaler(unittest.TestCase):
     def test_transform(self):
         # scaled_square = square[0] * 2
         # scaled_square[4, 12] = 1.
-        means = gs.localized_mean(square)
         scale = gs.scale(square)
         n_square = gs.transform(square, scale=scale)
         self.assertTrue((n_square == normalized_square).all())
+        coords = [geom[:, :2].flatten() for geom in n_square]
+        coords = [item for sublist in coords for item in sublist]
+        std = np.std(coords)
+        self.assertAlmostEqual(std, 1., 1)
+
+    def test_upsized_transform(self):
+        square_0 = square[0] * 2
+        square_0[:4, 2] = 1.
+        square_0[4, 4] = 1.
+        scale = gs.scale([square_0])
+        n_square = gs.transform([square_0], scale=scale)
+        self.assertTrue((n_square == normalized_square).all())
+        coords = [geom[:, :2].flatten() for geom in n_square]
+        coords = [item for sublist in coords for item in sublist]
+        std = np.std(coords)
+        self.assertAlmostEqual(std, 1., 1)
